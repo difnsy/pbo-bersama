71b4a434287d0edf8a1577c4e687d19317d6ffc7
Dani Ijlal Fitriansyah <155644977+difnsy@users.noreply.github.com>
2025-07-05 15:16:51 +0700

    Delete Before ES6 dan ES6 Js directory

--- Before ES6 dan ES6 Js/Before ES6.js
+++
@@ -1,8 +0,0 @@
-var person = {
-  name: "Dani",
-  sayHello: function() {
-    console.log("Halo, nama saya " + this.name);
-  }
-};
-
-person.sayHello(); // Output: Halo, nama saya Dani
--- Before ES6 dan ES6 Js/ES6.js
+++
@@ -1,8 +0,0 @@
-const person = {
-  name: "Dani",
-  sayHello() {
-    console.log(`Halo, nama saya ${this.name}`);
-  }
-};
-
-person.sayHello(); // Output: Halo, nama saya Dani
